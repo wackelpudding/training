cc82e9b0938c816ab4b2f907554e25a281f9bd78
Ricardo Jimenez Tuero <jimeneztuero@system.de>
2021-10-05 00:04:14 +0200

    .

---
+++ FiveTimes.py
@@ -0,0 +1,4 @@
+print('My Name is')
+name = input()
+for i in range(5):
+    print(name + ' Five Times (' + str(i) + ')')
---
+++ eqWhile.py
@@ -0,0 +1,6 @@
+print('My name is')
+i = 0
+name = input()
+while i < 5:
+    print(name + ' Five Times (' + str(i) + ')')
+    i += 1
